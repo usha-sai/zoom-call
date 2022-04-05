b6f6aa4e4d85c176af0271dbd773df3204d4077b
usha-sai <100438216+usha-sai@users.noreply.github.com>
2022-04-05 22:32:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,4 +26,9 @@ io.on('connection', socket => {
   })
 })
 
-server.listen(3000)
+const server = http.createServer(app);
+const port = 3000;
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+console.debug('Server listening on port ' + port);
